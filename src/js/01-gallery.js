208c57c46e037ad54c4de5ac331847845bedf92d
MariOkner <99042644+MariOkner@users.noreply.github.com>
2022-08-18 21:10:11 +0200

    подключила библиотеку SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,53 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+document.addEventListener('keydown', onKeyDown);
+const lightbox = basicLightbox.create('');
+
+const galleryEl = document.querySelector('.gallery');
+const galleryImages = createGallery(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryImages);
+
+galleryEl.addEventListener('click', onGalleryClick);
+
+function createGallery() {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <li class="gallery__list">
+            <a class="gallery__link" href="${original}">
+                <img 
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>    
+        </li>
+        `; 
+    }).join('');  
+}
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG') return;
+    
+    lightbox.element().innerHTML = 
+    `<img class="active"
+        src="${event.target.dataset.source}" width="800" height="600"
+    />`;
+    lightbox.show();
+}
+
+function onKeyDown(event) {
+    const ESC_KEY_CODE = 'Escape';
+    if (event.code === ESC_KEY_CODE) {
+        lightbox.close();
+    }
+}
+
