e37e854a4e503be7f35c30a926af1c8b6cfe29c7
MariOkner <99042644+MariOkner@users.noreply.github.com>
2022-08-23 21:19:23 +0200

    Зробила 8 ДЗ. друга попитка.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,9 +5,7 @@ import '../css/03-feedback.css';
 const FEEDBACK_KEY = 'feedback-form-state';
 const formEl = document.querySelector('.feedback-form');
 const mailEl = document.querySelector('input');
-const textareaEl = document.querySelector('textarea');
-
-const formData = {};
+const messageEl = document.querySelector('textarea');
 
 populateForm();
 
@@ -16,13 +14,22 @@ formEl.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormSubmit(event) {
     event.preventDefault();
+
+    let eventMailValue = event.target.elements.email.value;
+    let eventMessageValue = event.target.elements.message.value;
+    const UserObj = { email: eventMailValue, message: eventMessageValue};
+   
+    if (eventMailValue === '' || eventMessageValue === '') {   
+        return alert('All fields must be filled in');
+    }
+
     event.target.reset();
     localStorage.removeItem(FEEDBACK_KEY);
 };
 
 function onFormInput(event) {
+    const formData = localStorage.getItem(FEEDBACK_KEY) ? JSON.parse(localStorage.getItem(FEEDBACK_KEY)) : {};
     formData[event.target.name] = event.target.value;
-    console.log(formData);
     localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
 };
 
@@ -30,12 +37,7 @@ function populateForm() {
     const saveMessage = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
     if (saveMessage) {
         mailEl.value = saveMessage.email;
-        textareaEl.value = saveMessage.message
-        // console.log('message:', textareaEl.value = saveMessage.message);
+        messageEl.value = saveMessage.message;
     };
-   
-    // Object.keys(saveMessage).forEach(name => {
-    //     const inp = frm.querySelector(`[name="${name}"]`);
-    //     if (inp) inp.value = saveMessage[name];
-    // });
+    console.log(saveMessage);
 };
